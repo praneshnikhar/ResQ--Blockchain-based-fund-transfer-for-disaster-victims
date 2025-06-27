992af61dc2623aab65f91842857bc9d1f9fdd1fc
Pranesh Nikhar <praneshnikhar@gmail.com>
2025-06-27 22:27:07 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 // This page is deprecated and now redirects to the root login page.
 export default function LoginPageRedirect() {
   const router = useRouter();
+  
   useEffect(() => {
     router.replace('/');
   }, [router]);
